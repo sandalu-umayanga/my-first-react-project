77a4d60a999d4cb42b84496b38dc45d5405bfa15
sandalu umayanga <sandaluumayangaofficial@gmail.com>
2024-08-02 16:32:38 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import React from 'react';
 import './App.css';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import MainInfo from './Components/MainInfo';
 import Navigation from './Components/Navigation';
+import PatientLogin from './Components/PatientLogin';
 
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-      <MainInfo/>
-      <Home/>
-      <Footer/>
+      <Navigation />
+      <div className="main-content">
+        <MainInfo />
+        <PatientLogin />
+      </div>
+      <Footer />
     </div>
   );
 }
